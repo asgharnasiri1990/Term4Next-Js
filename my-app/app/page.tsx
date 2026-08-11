315b4c029ce97021d38bd34e5f748c90c2ce675e
asghar <asgharnasiri1990@gmail.com>
2026-08-12 00:39:33 +0330

    1st
    primary exercise for Next js

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+  <div className="flex flex-col flex-1 items-center justify-start font-san bg-[#081e3a]">
+      <h2 className="text-center text-3xl font-bold bg-[#c27987] ">Hello, World</h2>
+  </div>
+    </>
+  );
+}
