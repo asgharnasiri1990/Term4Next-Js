4ceef555f41688a4bc2d828f08d9e05646c12a59
asghar <asgharnasiri1990@gmail.com>
2026-08-20 08:54:14 +0330

    header

--- nextJs-turkishAirlines/app/page.tsx
+++ nextJs-turkishAirlines/app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-  <div className="flex flex-col flex-1 items-center justify-start font-san bg-[#081e3a]">
-      <h2 className="text-center text-3xl font-bold bg-[#c27987] ">Hello, World</h2>
-  </div>
+      <div className="bg-cover h-125 " style={{backgroundImage:"url('/hero.webp')"}}>
+
+        <div className="bg-[#333C49] h-8 w-full  "></div>
+        <div> </div>
+
+      </div>
     </>
   );
 }
