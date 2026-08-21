b6af1310a2cc4e3b34517be2a3877d64009de938
asghar <asgharnasiri1990@gmail.com>
2026-08-21 23:08:37 +0330

    navbar-responsive

--- nextJs-turkishAirlines/app/page.tsx
+++ nextJs-turkishAirlines/app/page.tsx
@@ -4,11 +4,12 @@ import TopLinks from "./TopLink";
 export default function Home() {
   return (
     <>
-      <div className="relative w-full h-137.5">
+      <div className="relative w-200 md:w-full h-137.5">
 
         {/* Hero Image */}
+
         <Image
-          src="/hero.webp"
+          src="/hero2.webp"
           alt="Turkish Airlines"
           fill
           priority
@@ -19,35 +20,41 @@ export default function Home() {
         <header className="absolute top-0 left-0 w-full">
 
           {/* Top Bar */}
-          <div className=" h-8 w-full bg-[#333C49] flex justify-end items-center text-[#c3cbd7] text-xs font-semibold pr-4 gap-6" >
+          <div className=" h-8 w-full bg-[#333C49] flex justify-end items-center text-[#c3cbd7] text-xs font-semibold pr-5 gap-3" >
 
             <TopLinks href="#" title="Turkish Airlines Holidays" className="hidden md:flex" />
             <TopLinks href="#" title="Corporate Club" className="hidden md:flex" />
             <TopLinks href="#" icon="/search.svg" title="Search" className="hidden md:flex" />
-            <TopLinks href="#" icon="/globe.svg" title="EN-TR" />
+            <TopLinks href="#" icon="/globe.svg" title="EN-TR" className="mr-2 " />
 
           </div>
 
           {/* Main Navigation */}
           <nav className="flex items-center h-15 w-full bg-[linear-gradient(180deg,#333c49,rgba(51,60,73,0)_110.83%)]">
 
-            <div className=" " >
-              <TopLinks href="#" icon="/turkishairlines.svg" iconWidth={190} iconHeight={30} className="ml-5" />
+            <div  >
+              <TopLinks href="#" icon="/turkishairlines.svg" iconWidth={190} iconHeight={30} className="ml-5 hidden md:block " />
+              <button className="md:hidden flex flex-col gap-1 ml-5">
+
+                <span className="w-6 h-0.5 bg-white"></span>
+                <span className="w-6 h-0.5 bg-white"></span>
+                <span className="w-6 h-0.5 bg-white"></span>
+              </button>
             </div>
 
             <div className="ml-auto flex items-center text-xs gap-5 font-black text-white ">
-              <TopLinks href="#" title="BOOK&manage" className="hover:bg-amber-300gre" />
-              <TopLinks href="#" title=" EXPERIENCE " />
-              <TopLinks href="#" title=" DETAILS&DESTINATIONS " />
-              <TopLinks href="#" title=" MILES&SMILES " />
-              <TopLinks href="#" title=" HELP " />
+              <TopLinks href="#" title="BOOK&MANAGE" className="hover:bg-amber-300gre hidden md:flex" />
+              <TopLinks href="#" title=" EXPERIENCE " className=" hidden md:flex" />
+              <TopLinks href="#" title=" DETAILS&DESTINATIONS " className=" hidden md:flex" />
+              <TopLinks href="#" title=" MILES&SMILES " className=" hidden md:flex" />
+              <TopLinks href="#" title=" HELP " className=" hidden md:flex" />
 
-              <div className=" flex h-10 w-25 items-center justify-center rounded-4xl border gap-2  hover:bg-gray-100 cursor-pointer">
+              <div className=" flex h-10 w-25 items-center justify-center md:rounded-4xl border gap-2  hover:bg-gray-400 cursor-pointer">
                 <Image src="/person.svg" alt="" width={15} height={15} />
                 <button className="text-md font-bold cursor-pointer "> sign-in</button>
 
               </div>
-              <Image src="/notification.svg" alt="" width={15} height={15} className="mr-5"/>
+              <Image src="/notification.svg" alt="" width={15} height={15} className="mr-5" />
             </div>
 
 
