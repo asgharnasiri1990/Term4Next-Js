d52f987ba0f840e391b2f4b0447454d9d5d47798
asghar <asgharnasiri1990@gmail.com>
2026-08-26 14:26:02 +0330

    mobile booking section Tabs
    Updated previous codes and added Mobile Section tabs by Shadcn components

--- nextJs-turkishAirlines/app/page.tsx
+++ nextJs-turkishAirlines/app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import TopLinks from "./TopLink";
+import TopLinks from "./components/TopLink";
 import Link from "next/link";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <>
-      <div className="relative   w-auto md:h-137.5 md:w-full  h-82  mb-96">
+      <div className="relative w-auto md:h-137.5 md:w-full  h-82  mb-96">
 
         {/* Hero Image */}
 
         <Image
-          src="/hero.webp"
+          src="/hero2.webp"
           alt="Turkish Airlines"
           fill
           priority
@@ -32,7 +33,7 @@ export default function Home() {
           </div>
 
           {/* Main Navigation */}
-          <nav className="flex items-center h-15 w-full bg-green-700 md:bg-[linear-gradient(180deg,#333c49,rgba(51,60,73,0)_110.83%)]">
+          <nav className="flex items-center h-15 w-full bg-[#232B38]  md:bg-transparent md:bg-[linear-gradient(180deg,#333c49,rgba(51,60,73,0)_110.83%)]">
 
             <div  >
               <TopLinks href="#" icon="/turkishairlines.svg" iconWidth={190} iconHeight={30} className="ml-5 hidden md:block " />
@@ -50,9 +51,9 @@ export default function Home() {
 
             </div>
 
-            <div className="ml-auto flex items-center text-xs gap-5 font-black text-white ">
+            <div className="ml-auto flex items-center text-xs gap-5 font-black text-white  hover:bg-green-400 ">
 
-              <TopLinks href="#" title="BOOK&MANAGE" className="hover:bg-amber-300gre hidden md:flex" />
+              <TopLinks href="#" title="BOOK&MANAGE" className= " hidden md:flex" />
               <TopLinks href="#" title=" EXPERIENCE " className=" hidden md:flex" />
               <TopLinks href="#" title=" DETAILS&DESTINATIONS " className=" hidden md:flex" />
               <TopLinks href="#" title=" MILES&SMILES " className=" hidden md:flex" />
@@ -72,7 +73,7 @@ export default function Home() {
         </header>
 
         <div className="absolute top-45 ml-2 md:top-38 md:h-42 w-[90%] md:w-[80%] max-w-220 md:left-1/2 md:-translate-x-1/2 flex flex-col gap-2 text-white text-left">
-          <Link rel="stylesheet" href="#" className="text-xl md:text-[38px] font-xs hidden md:block">
+          <Link  href="#" className="text-xl md:text-[38px]  hidden md:block">
             Choose your destination before summer ends
           </Link>
           <span className="md:hidden font-bold text-[24px]">Extend you summer now</span>
@@ -84,21 +85,57 @@ export default function Home() {
           <button className="sm:backdrop-blur-2xl h-12.75 w-32.5 sm:mx-0 font-semi-bold  md:font-semibold text-[14px] md:border rounded cursor-pointer hidden md:block">
             BOOK NOW
           </button>
-          <Link rel="stylesheet" href="#" className="text-[16px] font-semibold md:hidden" >BOOK NOW 	&gt; </Link>
+          <Link  href="#" className="text-[16px] font-semibold md:hidden" >BOOK NOW 	&gt; </Link>
 
         </div>
 
-        <div className="m-4 absolute top-75 bg-amber-300 h-72.5 w-74 md:hidden flex  flex-col gap-2 rounded">
-          <TopLinks href="#" icon="/flight.svg" title="Flight" iconWidth={30} iconHeight={60} className="bg-gray-500  h-11.25" />
-          <TopLinks href="#" icon="/flight.svg" title="Flight" iconWidth={30} iconHeight={60} className="bg-gray-500  h-11.25" />
-          <TopLinks href="#" icon="/flight.svg" title="Flight" iconWidth={30} iconHeight={60} className="bg-gray-500  h-11.25" />
-          <TopLinks href="#" icon="/flight.svg" title="Flight" iconWidth={30} iconHeight={60} className="bg-gray-500  h-11.25" />
-          <TopLinks href="#" icon="/flight.svg" title="Flight" iconWidth={30} iconHeight={60} className="bg-gray-500  h-11.25" />
-          <TopLinks href="#" icon="/flight.svg" title="Flight" iconWidth={30} iconHeight={60} className="bg-gray-500  h-11.25" />
-        </div>
+
+       
+        {/* Mobile Booker Section - Invisible in Desktop */}
+        <section className="absolute left-1/2 -translate-x-1/2 top-82 bg-amber-300 h-76 w-[80%] max-w-174 md:hidden flex flex-col justify-center rounded">
+
+          <Tabs defaultValue="flight" className="flex  gap-1 w-full ">
+            <TabsList className="h-82! flex flex-col items-baseline gap-1 w-full bg-transparent ">
+
+              {/*FLIGHT*/}
+              <TabsTrigger value="flight" className="flex -top-2  items-center justify-start gap-1 h-15 border w-full ">
+                <Image src="/flight.svg" alt="" width={24} height={15} style={{ width: 24, height: 13 }} /> 
+                <span className="text-red-500 text-xl font-bold">Flight</span>
+              </TabsTrigger>
+
+              <TabsTrigger value="flightHotel" className="flex items-center justify-start gap-1  h-15 -top-2 bg-red-400 w-full ">
+                <Image src="/flight-hotel.svg" alt="" width={18} height={18} style={{ width: 18, height: 18 }} />
+                <span className="text-xl">Flight + Hotel &amp; Tours</span>
+              </TabsTrigger>
+
+              <TabsTrigger value="checkin" className="flex  items-center justify-start gap-1  h-15 -top-2 bg-red-400  w-full">
+                <Image src="/checkin.svg" alt="" width={22} height={21} style={{ width: 22, height: 21 }} />
+                <span className="text-xl">Check-in</span>
+              </TabsTrigger>
+
+              <TabsTrigger value="manage" className="flex  items-center justify-start gap-1  h-15 -top-2 bg-red-400 w-full">
+                <Image src="/manage-booking.svg" alt="" width={18} height={17} style={{ width: 18, height: 17 }} />
+                <span className="text-xl">Manage booking</span>
+              </TabsTrigger>
+
+              <TabsTrigger value="status" className="flex  items-center justify-start gap-1  h-15 -top-2  bg-red-400  w-full">
+                <Image src="/flight-status.svg" alt="" width={24} height={16} style={{ width: 24, height: 16 }} />
+                <span className="text-xl ">Flight status</span>
+              </TabsTrigger>
+
+              <TabsTrigger value="award" className="flex  items-center justify-start gap-1  h-15 -top-2 bg-red-400  w-full">
+                <Image src="/award-ticket.svg" alt="" width={12} height={16} style={{ width: 12, height: 16 }} />
+                <span className="text-xl">Award ticket</span>
+              </TabsTrigger>
+
+            </TabsList>
+          </Tabs>
+
+        </section>
       </div>
 
 
     </>
-  );
+  )
 }
+;
