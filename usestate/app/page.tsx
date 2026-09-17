00a028f15053d7ea0b5d8a21936ed9f8e042a887
asghar <asgharnasiri1990@gmail.com>
2026-09-17 11:56:50 +0300

    usestate exercise
    how to change a counter with useState

---
+++ usestate/app/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(5)
+
+  const handleIncrease = () => {
+
+    setCount(count+1)
+  }
+  const handleDecrease =()=>{
+    setCount(prev => prev-1)
+  }
+  return (
+    <div className="flex flex-col gap-2  items-center p-5 justify-center text-xl">
+      <span>counter:{count} </span>
+      <button
+        className="cursor-pointer bg-green-400 rounded p-1"
+        onClick={handleIncrease}
+      >increase</button>
+
+      <button
+        className="cursor-pointer bg-green-400 rounded p-1"
+        onClick={handleDecrease}
+      >decrease</button>
+    </div>
+  );
+}
