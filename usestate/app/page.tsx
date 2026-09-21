c2be43eaf119df21d75645707b13da2ec2187b03
asghar <asgharnasiri1990@gmail.com>
2026-09-21 09:55:55 +0330

    extra files

--- usestate/app/page.tsx
+++ usestate/app/page.tsx
@@ -5,25 +5,26 @@ import { useState } from "react";
 export default function Home() {
   const [count, setCount] = useState(5)
 
-  const handleIncrease = () => {
+  // const handleIncrease = () => {
 
-    setCount(count+1)
-  }
-  const handleDecrease =()=>{
-    setCount(prev => prev-1)
-  }
+  //   setCount(count+1)
+  // }
+  // const handleDecrease =()=>{
+  //   setCount(prev => prev-1)
+  // }
   return (
     <div className="flex flex-col gap-2  items-center p-5 justify-center text-xl">
       <span>counter:{count} </span>
       <button
         className="cursor-pointer bg-green-400 rounded p-1"
-        onClick={handleIncrease}
-      >increase</button>
+        onClick={()=> setCount((prev)=> prev+1)}
+      >+</button>
 
       <button
         className="cursor-pointer bg-green-400 rounded p-1"
-        onClick={handleDecrease}
-      >decrease</button>
+        onClick={()=> setCount((prev)=>prev-1)}
+      >-</button>
     </div>
   );
 }
+ 
