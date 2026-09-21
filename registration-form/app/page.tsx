246fe44d5ab9435ea408dd67c9cd60f54debb514
asghar <asgharnasiri1990@gmail.com>
2026-09-21 09:56:24 +0330

    added delete account

--- registration-form/app/page.tsx
+++ registration-form/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { use, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
 
@@ -15,28 +15,39 @@ export default function Home() {
   const [password, setPassword] = useState("")
   const [passwordError, setPasswordError] = useState("")
 
-  //Click on Submit Function:
+  // sucessful submission
+  const [successMessage, setSuccessMessage] = useState("")
+
+
+  //Click on Submit Button Function:
   function handleSubmit() {
+    let isValid = true
 
     // Name conditions
     if (name.trim().length === 0) {
       setNameError("Name is required")
+      isValid = false
     }
     else if (name.length < 5) {
       setNameError("Name must be at least 5 characters")
+      isValid = false
     } else {
       setNameError("")
+
     }
 
     // Age conditions
-    if (age === "") {
+    if (age.trim() === "") {
       setAgeError("Age is required")
+      isValid = false
     }
     else if (isNaN(Number(age))) {
       setAgeError("Age must be a number")
+      isValid = false
     }
     else if (Number(age) < 18) {
       setAgeError("Age must be 18 or above. ")
+      isValid = false
     }
     else {
       setAgeError("")
@@ -44,28 +55,41 @@ export default function Home() {
 
     // Password conditions
     if (password === "") {
-      setPasswordError("Password is reqired")
+      setPasswordError("Password is required")
+      isValid = false
+
     }
     else if (password.length < 8) {
       setPasswordError("Password must be at least 8 characters")
+      isValid = false
     }
     else if (! /\d/.test(password)) {
       setPasswordError("Password must contain a number")
+      isValid = false
     }
     else if (!/[A-Z]/.test(password)) {
       setPasswordError("Password must contain an uppercase letter")
+      isValid = false
     }
-    else if(!/[a-z]/.test(password)){
+    else if (!/[a-z]/.test(password)) {
       setPasswordError("Password must contain a lowercase letter")
+      isValid = false
     }
     else if (!/[!@#$%^&*()]/.test(password)) {
       setPasswordError("Password must contain a special character")
+      isValid = false
     }
 
     else {
       setPasswordError("")
     }
 
+    if (isValid) {
+      setSuccessMessage("Form submitted Successfully")
+    }
+    else {
+      setSuccessMessage("Please check the form and follow intructions")
+    }
   }
 
 
@@ -75,7 +99,9 @@ export default function Home() {
 
 
     // User Interface
-    <div className="flex items-center justify-center pt-10 ">
+    
+    <div className="flex flex-col items-center justify-center pt-10  ">
+
       <div className="border rounded-full border-blue-300 w-auto">
 
         <div className="flex flex-col justify-center items-center border-white border w-150 h-110 p-5 gap-2 rounded-full bg-gray-200">
@@ -86,8 +112,9 @@ export default function Home() {
             </div>
           </div>
 
-          {/* Froms Inputs*/}
+          {/* Forms Inputs*/}
           <div className=" flex flex-col gap-5 p-4 ">
+
             {/* Name field */}
             <div className="flex flex-col gap-1">
               <input
@@ -103,18 +130,15 @@ export default function Home() {
               <input
                 value={age}
                 onChange={(e) => setAge(e.target.value)}
-                type="text"
+                type="number"
                 placeholder="Age:"
                 className="border bg-gray-100 w-80 h-10 rounded-full p-3 hover:scale-102 placeholder:text-xs transition-transform" />
-
               {ageError && (<p className="text-red-500 pl-3 text-xs"> {ageError} </p>)}
             </div>
 
-
             {/* Password Field */}
             <div>
-
-              <input type="text"
+              <input type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter Password:"
@@ -123,14 +147,19 @@ export default function Home() {
             </div>
 
             {/*Submit Button */}
-            <div className="flex justify-center">
+            <div className="flex flex-col gap-2 justify-center">
               <div className="bg-gray-300 p-2 rounded-full items-center font-bold text-center text-xl ">
                 <button
                   onClick={handleSubmit}
-
+                  onChange={(e) => {
+                    setName(e.target.value)
+                    setSuccessMessage("")
+                  }}
                   className="bg-gray-100 w-30 items-end cursor-pointer  rounded-full p-1 hover:scale-110 active:bg-red-300 transition-transform">Submit</button>
               </div>
+              {successMessage && (<p className="text-red-500 pl-3 text-xs"> {successMessage} </p>)}
             </div>
+
           </div>
 
         </div>
